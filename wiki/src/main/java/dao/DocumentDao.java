7d38a7bf204094eaf6711b2ab614aee74fef08d7
kimmmyung <rlaaudgh1003@daum.net>
2022-05-25 16:45:36 +0900

    제목 중복 체크 구현

--- wiki/src/main/java/dao/DocumentDao.java
+++ wiki/src/main/java/dao/DocumentDao.java
@@ -1,5 +1,24 @@
 package dao;
 
-public class DocumentDao {
+public class DocumentDao extends Dao{
 
+	public DocumentDao() {
+		super(); // 부모클래스 생성자 호출 
+	}
+	public static DocumentDao documentDao = new DocumentDao(); 		// dao 호출시 반복되는 new 연산자 제거 
+	public static DocumentDao getdocumentDao() { return documentDao; } 
+	
+	public boolean titlecheck(String dtitle) {
+		try {
+			String sql = "select * from document where dtitle = ?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, dtitle);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				return true;
+			}
+		}catch(Exception e) {e.printStackTrace();}
+		
+		return false;
+	}
 }
