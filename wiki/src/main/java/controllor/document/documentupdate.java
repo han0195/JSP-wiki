123302c4dbf92ab0a23be27d4049281c2485c050
jeost <jjy77jjy@gmail.com>
2022-06-09 11:56:09 +0900

    쓸모없는 부분 제거

--- wiki/src/main/java/controllor/document/documentupdate.java
+++ wiki/src/main/java/controllor/document/documentupdate.java
@@ -53,31 +53,7 @@ public class documentupdate extends HttpServlet {
 			
 			String dcontent = jo.getString("dcontent");
 			int dno = jo.getInt("dno");
-			String linkTitle="";
 				
-				
-				
-		//정규표현식으로 링크거는게 있는지 판별
-		if(dcontent.matches("^*[[(.*?)]]*")) { System.out.println("링크 존재");
-			//있을경우 [[ ]] 내부의 단어 추출
-			Pattern pattern=Pattern.compile("^*[[(.*?)]]*");
-			Matcher matcher=pattern.matcher(dcontent);
-			while(matcher.find()) {
-				System.out.println(matcher.group(1));
-				linkTitle=matcher.group(1);
-				// 추출한 단어를 넣어서 해당하는 링크할 제목의 문서 번호 호출
-				dno= DocumentDao.getdocumentDao().getdno(linkTitle);
-				if(dno==-1) { // 해당하는 제목의 문서가 없다면
-					System.out.println("없는 문서를 링크하려 시도함."); continue;
-				}else { // 해당하는 제목의 문서가 있다면
-					//역링크 목록에 추가해주기
-					// SpecialDao.getSpecialDao().addLink(현재 페이지 번호, dno);
-				}
-				if(matcher.group(1)==null) { // 정규표현식에 해당하는 문자열이 더이상 없다면
-					break;
-				}
-			} // while e
-		} //if e
 			String dimg = jo.getString("dimgname");
 			
 			if(mid == null) {
